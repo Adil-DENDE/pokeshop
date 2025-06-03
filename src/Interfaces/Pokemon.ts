699461b75e24608b0c6fd34eb2fb1f80eeea12a8
Adil-DENDE <114158101+Adil-DENDE@users.noreply.github.com>
2025-06-03 22:30:27 +0200

    Add favorites pokedex feature

--- src/Interfaces/Pokemon.ts
+++ src/Interfaces/Pokemon.ts
@@ -1,95 +1,102 @@
 export class Pokemon {
-    id: number;
-    name: string;
-    baseExperience: number;
-    height: number;
-    weight: number;
-    isDefault: boolean;
-    abilities: Ability[];
-    cries: Cries;
-    locationAreaEncounters: string;
-    species: Species;
-    sprites: Sprite;
-    stats: Stat[];
-    types: Type[];
+  id: number
+  name: string
+  baseExperience: number
+  height: number
+  weight: number
+  isDefault: boolean
+  abilities: Ability[]
+  cries: Cries
+  locationAreaEncounters: string
+  species: Species
+  sprites: Sprite
+  stats: Stat[]
+  types: Type[]
 
-    constructor(data: Pokemon) {
-        this.id = data.id;
-        this.name = data.name;
-        this.baseExperience = data.baseExperience;
-        this.height = data.height;
-        this.weight = data.weight;
-        this.isDefault = data.isDefault;
-        this.abilities = data.abilities;
-        this.cries = data.cries;
-        this.locationAreaEncounters = data.locationAreaEncounters;
-        this.species = data.species;
-        this.sprites = data.sprites;
-        this.stats = data.stats;
-        this.types = data.types;
-    }
+  constructor(data: Pokemon) {
+    this.id = data.id
+    this.name = data.name
+    this.baseExperience = data.baseExperience
+    this.height = data.height
+    this.weight = data.weight
+    this.isDefault = data.isDefault
+    this.abilities = data.abilities
+    this.cries = data.cries
+    this.locationAreaEncounters = data.locationAreaEncounters
+    this.species = data.species
+    this.sprites = data.sprites
+    this.stats = data.stats
+    this.types = data.types
+  }
 
-    getSprite(): string | null {
-        return this.sprites.front_default;
-    }
+  getSprite(): string | null {
+    return this.sprites.front_default
+  }
 
-    getTypes(): string[] {
-        return this.types.map(type => type.type.name);
-    }
+  getTypes(): string[] {
+    return this.types.map((type) => type.type.name)
+  }
 
-    getAbilities(): string[] {
-        return this.abilities.map(ability => ability.ability.name);
-    }
+  getAbilities(): string[] {
+    return this.abilities.map((ability) => ability.ability.name)
+  }
 }
 
 interface Ability {
-    ability: {
-        name: string;
-        url: string;
-    };
-    is_hidden: boolean;
-    slot: number;
+  ability: {
+    name: string
+    url: string
+  }
+  is_hidden: boolean
+  slot: number
 }
 
 interface Cries {
-    latest: string;
-    legacy: string;
+  latest: string
+  legacy: string
 }
 
 interface Species {
-    name: string;
-    url: string;
+  name: string
+  url: string
 }
 
 interface Sprite {
-    back_default: string
-    back_female: string
-    back_shiny: string
-    back_shiny_female: string
-    front_default: string
-    front_female: string
-    front_shiny: string
-    front_shiny_female: string
+  back_default: string
+  back_female: string
+  back_shiny: string
+  back_shiny_female: string
+  front_default: string
+  front_female: string
+  front_shiny: string
+  front_shiny_female: string
 }
 
 interface Stat {
-    base_stat: number;
-    effort: number;
-    stat: {
-        name: string;
-        url: string;
-    };
+  base_stat: number
+  effort: number
+  stat: {
+    name: string
+    url: string
+  }
 }
 
 interface Type {
-    slot: number;
-    type: {
-        name: string;
-        url: string;
-    };
+  slot: number
+  type: {
+    name: string
+    url: string
+  }
 }
 
 export interface NotFullDataPokemon {
-    name: string
-    url: string
+  name: string
+  url: string
+}
+
+export interface FavoritePokemon {
+  id: number
+  name: string
+  image: string
+  type: string
 }
--- src/components/ui/PokemonCard.vue
+++ src/components/ui/PokemonCard.vue
@@ -1,24 +1,55 @@
 <template>
-    <div class="card bg-neutral shadow-md rounded-lg p-4 animate-fade">
-        <div class="flex justify-center">
-            <img :src="image" alt="Pokémon" class="w-30 h-full object-cover rounded-lg" />
-        </div>
-        <div class="mt-4 text-center">
-            <h2 class="text-lg text-warning font-bold">{{ props.name?.toLocaleUpperCase() }}</h2>
-            <p class="text-white">Type: {{ type }}</p>
-        </div>
-        <button class="btn btn-circle btn-warning mt-4 w-full">
-            Ajouter aux Favoris
-        </button>
+  <div class="card bg-neutral shadow-md rounded-lg p-4 animate-fade">
+    <div class="flex justify-center">
+      <img :src="image" alt="Pokémon" class="w-30 h-full object-cover rounded-lg" />
     </div>
+    <div class="mt-4 text-center">
+      <h2 class="text-lg text-warning font-bold">{{ props.name?.toLocaleUpperCase() }}</h2>
+      <p class="text-white">Type: {{ type }}</p>
+    </div>
+    <button
+      v-if="!props.isFavorite"
+      class="btn btn-circle btn-warning mt-4 w-full"
+      @click="addToFavorites"
+    >
+      Ajouter aux Favoris
+    </button>
+    <button v-else class="btn btn-circle btn-error mt-4 w-full" @click="removeFromFavorites">
+      Retirer
+    </button>
+  </div>
 </template>
 
 <script setup lang="ts">
+import { usePokedexStore } from '@/stores/pokedex'
+
 const props = defineProps({
-    name: String,
-    image: String,
-    type: String,
+  id: Number,
+  name: String,
+  image: String,
+  type: String,
+  isFavorite: {
+    type: Boolean,
+    default: false,
+  },
 })
 
+const pokedex = usePokedexStore()
 
+const addToFavorites = () => {
+  if (props.id && props.name && props.image && props.type) {
+    pokedex.add({
+      id: props.id,
+      name: props.name,
+      image: props.image,
+      type: props.type,
+    })
+  }
+}
+
+const removeFromFavorites = () => {
+  if (props.id) {
+    pokedex.remove(props.id)
+  }
+}
 </script>
